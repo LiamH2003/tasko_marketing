ac653f08d9ed041e900a5d46f1d64e8115e1fcc4
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2026-03-12 10:40:49 +0100

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,37 @@
 
 
+import taskoTrackerLogo from "../assets/TaskoTrackerLogo.svg";
+import githubLogo from "../assets/Octicons-mark-github.svg.png";
+import linkedInLogo from "../assets/LinkedIn_Logo.svg.png";
+import xLogo from "../assets/X_logo_2023.svg.png";
+
 export const Footer = () => {
   const footerLinks = [
-    { text: "Accessibility statement" },
-    { text: "Cookie preferences" },
-    { text: "Privacy Policy" },
+    { text: "Accessibility statement", href: "/accessibility-statement" },
+    { text: "Cookie preferences", href: "/cookie-preferences" },
+    { text: "Privacy Policy", href: "/privacy-policy" },
+  ];
+  const socialLinks = [
+    { alt: "GitHub", href: "#", src: githubLogo },
+    { alt: "LinkedIn", href: "#", src: linkedInLogo },
+    { alt: "X", href: "#", src: xLogo },
   ];
 
   return (
-    <footer className="flex w-full items-center gap-4 px-10 py-0 relative flex-[0_0_auto] bg-tasko-white100 border-t-line [border-top-style:solid] [border-right-style:none] [border-bottom-style:none] [border-left-style:none] border-tasko-blue400">
-      <div className="flex flex-col items-start gap-4 px-0 py-6 relative flex-1 grow">
+    <footer className="w-full px-6 md:px-10 py-0 relative flex-[0_0_auto] bg-tasko-white100 border-t-[0.5px] [border-top-style:solid] [border-right-style:none] [border-bottom-style:none] [border-left-style:none] border-tasko-blue400">
+      <div className="absolute top-6 left-6 md:left-10 inline-flex items-center gap-4">
+        <img
+          className="relative h-10 w-auto"
+          src={taskoTrackerLogo}
+          alt="TaskoTracker logo"
+        />
+        <span className="whitespace-nowrap text-body-large text-tasko-gray400">
+          Organize your routines efficiently with TaskoTracker
+        </span>
+      </div>
+
+      <div className="flex flex-col items-start gap-4 px-0 py-6 pt-24 pb-24 relative flex-1 grow">
         <div className="flex flex-col items-start gap-8 relative self-stretch w-full flex-[0_0_auto]">
-          <div className="inline-flex items-center justify-center gap-4 relative flex-[0_0_auto]">
-            <div className="flex flex-col w-[42.95px] h-[39.9px] items-start relative">
-              <div className="relative self-stretch w-full aspect-[1.23] bg-[url(/group.png)] bg-[100%_100%]" />
-            </div>
-
-            <div className="relative w-[137px] h-7 text-header-3 text-tasko-gray900">
-            Tasko tracker
-          </div>
-          </div>
-
           <nav
             className="flex w-[533px] items-center gap-6 relative flex-[0_0_auto]"
             aria-label="Footer navigation"
@@ -28,28 +39,40 @@ export const Footer = () => {
             {footerLinks.map((link, index) => (
               <a
                 key={index}
-                href="#"
+                href={link.href}
                 className="inline-flex gap-2.5 flex-[0_0_auto] items-center relative"
               >
-                <span className="flex w-fit mt-[-1.00px] text-body-large text-tasko-gray400 items-center relative">
-                  Links
+                <span className="flex w-fit mt-[-1.00px] text-header-4 text-tasko-gray400 items-center relative">
+                  {link.text}
                 </span>
               </a>
             ))}
           </nav>
         </div>
 
-        <div className="flex h-[53.67px] items-start justify-between px-0 py-4 relative self-stretch w-full border-t-line [border-top-style:solid] border-tasko-white700">
+        <div className="flex h-[53.67px] items-start justify-between px-0 py-4 relative self-stretch w-full border-t-[0.5px] [border-top-style:solid] border-tasko-white700">
           <div className="relative flex-1 grow h-[21px]">
-            <p className="w-[185px] mt-[-1.00px] text-body-main text-tasko-gray400 relative">
-            © 2024 tasko tracker
+            <p className="w-[220px] mt-[-1.00px] text-body-large text-tasko-gray400 relative">
+            © 2026 tasko tracker
           </p>
           </div>
 
-          <img
-            className="relative w-[108px] h-5"
-            alt="Social media icons"
-          />
+          <div className="inline-flex items-center justify-end gap-6 absolute bottom-6 right-6 md:right-10">
+            {socialLinks.map((socialLink) => (
+              <a
+                key={socialLink.alt}
+                href={socialLink.href}
+                aria-label={socialLink.alt}
+                className="inline-flex items-center justify-center relative flex-[0_0_auto]"
+              >
+                <img
+                  className="relative h-5 w-auto"
+                  src={socialLink.src}
+                  alt={socialLink.alt}
+                />
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
